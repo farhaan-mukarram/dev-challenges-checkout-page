7a0d0f3f387b363ad63bced18a197eeb7273f05f
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-09-09 21:24:16 +0500

    refactor: use array of items to render cart items in the Checkout Card

--- src/components/CheckoutCard/index.tsx
+++ src/components/CheckoutCard/index.tsx
@@ -4,63 +4,58 @@ import leviShoesPhoto from "../../assets/images/photo2.png";
 import addIcon from "../../assets/icons/add.svg";
 import removeIcon from "../../assets/icons/remove.svg";
 
+const cartItems = [
+  {
+    id: 1,
+    title: "Vintage Backpack",
+    unitPrice: "$94.99",
+    discountedUnitPrice: "$54.99",
+    image: vintageBackpackPhoto,
+  },
+  {
+    id: 1,
+    title: "Levi Shoes",
+    unitPrice: "$124.99",
+    discountedUnitPrice: "$74.99",
+    image: leviShoesPhoto,
+  },
+];
+
 const CheckoutCard = () => {
   return (
     <section className="bg-[#F2F2F2] p-8 rounded-xl h-max space-y-24">
       <div className="flex flex-col space-y-8">
-        <div className="flex space-x-5">
-          <img
-            src={vintageBackpackPhoto}
-            alt=""
-            className="w-32 h-full rounded-xl"
-          />
+        {cartItems.map(
+          ({ id, title, unitPrice, discountedUnitPrice, image }) => (
+            <div className="flex space-x-5" key={id}>
+              <img src={image} alt={title} className="w-32 h-full rounded-xl" />
 
-          <div className="flex flex-col justify-between w-full">
-            <div className="flex flex-col space-y-2">
-              <h4 className="text-base text-[#4E5150]">Vintage Backpack</h4>
-              <p>
-                <span className="text-[#F2994A]">$54.99</span>{" "}
-                <span className="line-through text-xs text-[#4E5150]">
-                  $94.99
-                </span>
-              </p>
-            </div>
-            <div className="flex justify-between p-3 border border-[#828282] rounded-xl">
-              <div className="w-6 h-6">
-                <img src={removeIcon} alt="" />
-              </div>
-              <div>1</div>
-              <div className="w-6 h-6">
-                <img src={addIcon} alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
+              <div className="flex flex-col justify-between w-full">
+                <div className="flex flex-col space-y-2">
+                  <h4 className="text-base text-[#4E5150]">{title}</h4>
+                  <p>
+                    <span className="text-[#F2994A]">
+                      {discountedUnitPrice}
+                    </span>{" "}
+                    <span className="line-through text-xs text-[#4E5150]">
+                      {unitPrice}
+                    </span>
+                  </p>
+                </div>
 
-        <div className="flex space-x-5">
-          <img src={leviShoesPhoto} alt="" className="w-32 h-full rounded-xl" />
-
-          <div className="flex flex-col justify-between w-full">
-            <div className="flex flex-col space-y-2">
-              <h4 className="text-base text-[#4E5150]">Levi Shoes</h4>
-              <p>
-                <span className="text-[#F2994A]">$74.99</span>{" "}
-                <span className="line-through text-xs text-[#4E5150]">
-                  $124.99
-                </span>
-              </p>
-            </div>
-            <div className="flex justify-between p-3 border border-[#828282] rounded-xl">
-              <div className="w-6 h-6">
-                <img src={removeIcon} alt="" />
-              </div>
-              <div>1</div>
-              <div className="w-6 h-6">
-                <img src={addIcon} alt="" />
+                <div className="flex justify-between p-3 border border-[#828282] rounded-xl">
+                  <div className="w-6 h-6">
+                    <img src={removeIcon} alt="" />
+                  </div>
+                  <div>1</div>
+                  <div className="w-6 h-6">
+                    <img src={addIcon} alt="" />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          )
+        )}
       </div>
 
       <div className="flex flex-col space-y-4">
