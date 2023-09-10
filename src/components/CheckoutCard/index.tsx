178ed3d567958e762bb93ed0f57b9baa2d85bbe4
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-09-10 15:36:58 +0500

    feat: add functionality to increase/decrease item quanity

--- src/components/CheckoutCard/index.tsx
+++ src/components/CheckoutCard/index.tsx
@@ -1,32 +1,90 @@
+import { useState } from "react";
+
 import vintageBackpackPhoto from "../../assets/images/photo1.png";
 import leviShoesPhoto from "../../assets/images/photo2.png";
 
 import addIcon from "../../assets/icons/add.svg";
 import removeIcon from "../../assets/icons/remove.svg";
 
-const cartItems = [
+type CartItemType = {
+  id: number;
+  title: string;
+  unitPrice: string | number;
+  discountedUnitPrice: string | number;
+  image: string;
+  quantity: number;
+};
+
+const DEFAULT_CART_ITEMS: Array<CartItemType> = [
   {
     id: 1,
     title: "Vintage Backpack",
     unitPrice: "$94.99",
     discountedUnitPrice: "$54.99",
     image: vintageBackpackPhoto,
+    quantity: 1,
   },
   {
-    id: 1,
+    id: 2,
     title: "Levi Shoes",
     unitPrice: "$124.99",
     discountedUnitPrice: "$74.99",
     image: leviShoesPhoto,
+    quantity: 1,
   },
 ];
 
 const CheckoutCard = () => {
+  const [cartItems, setCartItems] =
+    useState<Array<CartItemType>>(DEFAULT_CART_ITEMS);
+
+  const decreaseItemQuantity = (itemId: number) => {
+    setCartItems((prevCartItems) => {
+      const tempCartItems: Array<CartItemType> = JSON.parse(
+        JSON.stringify(prevCartItems)
+      );
+
+      const updatedCartItems = tempCartItems?.map((item) => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            quantity: item.quantity - 1 > 1 ? item.quantity - 1 : 1,
+          };
+        }
+
+        return item;
+      });
+
+      return updatedCartItems;
+    });
+  };
+
+  const increaseItemQuantity = (itemId: number) => {
+    setCartItems((prevCartItems) => {
+      const tempCartItems: Array<CartItemType> = JSON.parse(
+        JSON.stringify(prevCartItems)
+      );
+
+      const updatedCartItems = tempCartItems?.map((item) => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          };
+        }
+
+        return item;
+      });
+
+      return updatedCartItems;
+    });
+  };
+
   return (
     <section className="bg-[#F2F2F2] p-8 rounded-xl h-max space-y-24">
       <div className="flex flex-col space-y-8">
         {cartItems.map(
-          ({ id, title, unitPrice, discountedUnitPrice, image }) => (
+          ({ id, title, unitPrice, discountedUnitPrice, image, quantity }) => (
             <div className="flex space-x-5" key={id}>
               <img src={image} alt={title} className="w-32 h-full rounded-xl" />
 
@@ -44,13 +102,20 @@ const CheckoutCard = () => {
                 </div>
 
                 <div className="flex justify-between p-3 border border-[#828282] rounded-xl">
-                  <div className="w-6 h-6">
+                  <button
+                    className="w-6 h-6"
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    onClick={() => decreaseItemQuantity(id)}
+                  >
                     <img src={removeIcon} alt="" />
-                  </div>
-                  <div>1</div>
-                  <div className="w-6 h-6">
+                  </button>
+                  <div>{quantity}</div>
+                  <button
+                    className="w-6 h-6"
+                    onClick={() => increaseItemQuantity(id)}
+                  >
                     <img src={addIcon} alt="" />
-                  </div>
+                  </button>
                 </div>
               </div>
             </div>
